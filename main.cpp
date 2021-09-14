7cebbf2e699c437e84b6c8cab59b8897abe53175
Mark Fontenot <mfonten@lyle.smu.edu>
2021-09-14 05:29:25 -0500

    added output file name to be printed

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char** argv) {
     }
     else {
         std::cout << "Hello, 2341 students!" << std::endl;
+        std::cout << "Attempting to open and write to " << argv[2] << std::endl;
         std::ofstream o(argv[2]);
         o << "Writing to output file." << std::endl;
         o.close();
