3b7e5b546621e04771aa785b03d66650ffa5c83b
Mark Fontenot <mfonten@lyle.smu.edu>
2021-09-14 05:18:37 -0500

    added all necessary files for template for Fall 2021 CS 2341 Project 02

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/**
+ * Fall 2021 PA 02 Template Repo.
+ */
+
+#include <iostream>
+#include <fstream>
+#include "catch_setup.h"
+
+int main(int argc, char** argv) {
+    if(argc == 1) {
+        runCatchTests();
+    }
+    else {
+        std::cout << "Hello, 2341 students!" << std::endl;
+        std::ofstream o(argv[2]);
+        o << "Writing to output file." << std::endl;
+        o.close();
+    }
+    return 0;
+}
