d4ab34c81c69737f156ce62258e3b8de258c745a
Mark Fontenot <mfonten@lyle.smu.edu>
2021-09-14 06:53:15 -0500

    Added explanatory comments to some of the files.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 
 #include <iostream>
 #include <fstream>
+
+/**
+ * catch_setup.h and catch_setup.cpp contain the #define directive for
+ * the CATCH2 framework.  You can see in main below how I'm calling the
+ * runCatchTests() function which is declared in this header.
+ *
+ * On average, this should reduce the build time for your project on your local
+ * machine.
+ */
 #include "catch_setup.h"
 
 int main(int argc, char** argv) {
